f6d852902bc379bb3b734221f94d61bec103abe7
KhushiVikam <kikivikam@gmail.com>
2024-07-31 00:29:51 +0530

    Booklist creates

--- src/index.js
+++ src/index.js
@@ -1,10 +1,42 @@
 import React from "react";
-import ReactDOM from 'react-dom/cient';
+import ReactDOM from "react-dom/client";
 
-function Greeting(){
-  return <h2>MY First Component</h2>;
-}
+import "./index.css";
+const books = [
+  {
+    author: "Hillbilly Elegy",
+    title: "A Memoir of a Family and Culture in Crisis",
+    img: "./images/bk1.jpg",
+    id: 1,
+  },
+  {
+    author: "Colleen Hoover",
+    title: "It Ends with Us",
+    img: "https://images-na.ssl-images-amazon.com/images/I/813aV273-rL._AC_UL600_SR600,400_.jpg",
+    id: 2,
+  },
+];
 
-const root = ReactDOM.createRoot(document.getElementById('root'))
+const BookList = () => {
+  return (
+    <section className="booklist">
+      {books.map((book) => {
+        return <Book {...book} key={book.id} />;
+      })}
+    </section>
+  );
+};
 
-root.render(<Greeting />)
+const Book = ({ img, title, author }) => {
+  return (
+    <article className="book">
+      <img src={img} alt={title} />
+      <h2>{title}</h2>
+      <h4>{author}</h4>
+    </article>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<BookList />);
