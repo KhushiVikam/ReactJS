a1d8a0659f2c16f585a89d75b5f14bc4ebf51884
KhushiVikam <kikivikam@gmail.com>
2024-07-31 22:19:55 +0530

    Changes updated

---
+++ src/books.js
@@ -0,0 +1,24 @@
+import img1 from "./images/book1.jpg";
+import img2 from "./images/book2.jpg";
+import img3 from "./images/book3.jpg";
+
+export const books = [
+  {
+    author: "Hillbilly Elegy",
+    title: "A Memoir of a Family and Culture in Crisis",
+    img: img1,
+    id: 1,
+  },
+  {
+    author: "Colleen Hoover",
+    title: "It Ends with Us",
+    img: img2,
+    id: 2,
+  },
+  {
+    author: "James Clear",
+    title: "Atomic Habits",
+    img: img3,
+    id: 3,
+  },
+];
--- src/index.js
+++ src/index.js
@@ -2,38 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 import "./index.css";
-const books = [
-  {
-    author: "Hillbilly Elegy",
-    title: "A Memoir of a Family and Culture in Crisis",
-    img: "./images/bk1.jpg",
-    id: 1,
-  },
-  {
-    author: "Colleen Hoover",
-    title: "It Ends with Us",
-    img: "https://images-na.ssl-images-amazon.com/images/I/813aV273-rL._AC_UL600_SR600,400_.jpg",
-    id: 2,
-  },
-];
-
+import { books } from "./books";
+import Book from "./Book";
 const BookList = () => {
   return (
-    <section className="booklist">
-      {books.map((book) => {
-        return <Book {...book} key={book.id} />;
-      })}
-    </section>
-  );
-};
-
-const Book = ({ img, title, author }) => {
-  return (
-    <article className="book">
-      <img src={img} alt={title} />
-      <h2>{title}</h2>
-      <h4>{author}</h4>
-    </article>
+    <>
+      <h1> Amazon Best Seller</h1>
+      <section className="booklist">
+        {books.map((book, index) => {
+          return <Book {...book} key={book.id} number={index} />;
+        })}
+      </section>
+    </>
   );
 };
 
